312dea20a211307061d63634310ba1fcb8c584be
unknown <dueduedudu02@gmail.com>
2025-04-24 17:09:46 -0300

    first commit not 100%

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<script setup>
+import CarComponent from './components/CarComponent.vue'
+</script>
+
+<template>
+  <main class="main-box">
+    <CarComponent class="orangeBack">
+      <img src="/src/components/icons/icon-sedans.svg">
+      <h1>Sedans</h1>
+      <p>Choose a sedan for its affordability and excellent fuel economy.
+        Ideal for cruising in the city or on your next road trip.</p>
+      <button>Learn More</button>
+    </CarComponent>
+    <CarComponent class="darkcyanBack">
+      <img src="/src/components/icons/icon-suvs.svg">
+      <h1>SUVs</h1>
+      <p>Take an SUV for its spacious interior, power, and versatility.
+        Perfect for your next family vacation and off-road adventures.</p>
+      <button>Learn More</button>
+    </CarComponent>
+    <CarComponent class="verydarkcyanBack">
+        <img src="/src/components/icons/icon-luxury.svg">
+        <h1>Luxury</h1>
+        <p>Cruise in the best car brands without the bloated prices.
+          Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
+        <button>Learn More</button>
+    </CarComponent>
+
+  </main>
+</template>
+
+<style scoped>
+.main-box {
+  display: flex;
+  width: 800px;
+  height: auto;
+}
+
+
+
+.main-box h1 {
+  font-family: 'Big Shoulders';
+  text-transform: uppercase;
+  color: hsl(0, 0%, 95%);
+  margin-top: 25px;
+}
+
+.main-box p {
+  font-family: 'Lexend Deca';
+  font-size: 13px;
+  line-height: 1.5rem;
+  color: hsla(0, 0%, 100%, 0.75);
+  margin-top: 25px;
+}
+
+.main-box button {
+  width: 120px;
+  height: 42px;
+  border: none;
+  border-radius: 50px;
+  font-family: 'Lexend Deca';
+  cursor: pointer;
+  margin-top: 60px;
+}
+
+.main-box button:hover {
+  background: transparent;
+  color: hsl(0, 0%, 95%);
+  border: 2px solid hsl(0, 0%, 95%);
+}
+
+.orangeBack button {
+  color: hsl(31, 77%, 52%);
+}
+
+.darkcyanBack button {
+  color: hsl(184, 100%, 22%);
+}
+
+.verydarkcyanBack button {
+  color: hsl(179, 100%, 13%);
+}
+
+@media (max-width: 800px) {
+  .main-box {
+    flex-direction: column;
+    width: 80vw;
+    height: 85vh;
+  }
+}
+</style>
---
+++ src/components/CarComponent.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    props: {
+        colorClass: {
+            type: String,
+            default: 'orangeBack'
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="[colorClass]">
+        <slot></slot>
+    </div>
+
+</template>
+
+<style scoped>
+.orangeBack {
+    background-color: hsl(31, 77%, 52%);
+    width: fit-content;
+    border-radius: 6px 0 0 6px;
+    padding: 42px;
+}
+
+.darkcyanBack {
+    background-color: hsl(184, 100%, 22%);
+    width: fit-content;
+    padding: 42px;
+}
+
+.verydarkcyanBack {
+    background-color: hsl(179, 100%, 13%);
+    width: fit-content;
+    border-radius: 0 6px 6px 0;
+    padding: 42px;
+}
+
+@media (max-width: 800px) {
+    .orangeBack {
+        border-radius: 6px 6px 0 0;
+
+    }
+
+    .verydarkcyanBack {
+        border-radius: 0 0 6px 6px;
+    }
+}
+</style>
